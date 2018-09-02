88d02d7f2cd8681284677643a40d1ee8f923b225
sohnryang <loop.infinitely@gmail.com>
2018-09-02 02:21:51 +0000

    added box vs box collision check

---
+++ collision.py
@@ -0,0 +1,17 @@
+"""
+collision.py -- collision check functions
+
+"""
+
+from box import Box
+
+def box_vs_box(box1, box2):
+    """
+    Check collision between two boxes.
+
+    """
+    if box1.max_p.x < box2.min_p.x or box1.min_p.x > box2.max_p.x:
+        return False
+    if box1.max_p.y < box2.min_p.y or box1.min_p.y > box2.max_p.y:
+        return False
+    return True
