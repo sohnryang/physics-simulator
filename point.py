0f254d422e7345308b19306a56e72008122fead7
sohnryang <loop.infinitely@gmail.com>
2018-09-02 02:13:56 +0000

    added point

---
+++ point.py
@@ -0,0 +1,18 @@
+"""
+point.py -- a point
+
+"""
+
+
+class Point:
+    """
+    Point -- a class that represents a point
+
+    """
+    def __init__(self, x, y):
+        """
+        Initalize a point in the given cordinate.
+
+        """
+        self.x = x
+        self.y = y
