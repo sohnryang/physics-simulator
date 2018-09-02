73731c97d023ac0686cf44522fb56ac1e2ef7be9
sohnryang <loop.infinitely@gmail.com>
2018-09-02 22:29:15 +0000

    added circle

---
+++ circle.py
@@ -0,0 +1,18 @@
+"""
+circle.py -- a circle
+
+"""
+
+
+class Cirle:
+    """
+    Circle -- a circle
+
+    """
+    def __init__(self, radius, position):
+        """
+        Initialize a circle.
+
+        """
+        self.radius = radius
+        self.position = position
